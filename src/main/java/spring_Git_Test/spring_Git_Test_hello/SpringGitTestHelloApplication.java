eac8d4e0ac0342aa081c298beea202480137c1d5
wnghks517 <wnghks517@gmail.com>
2023-04-15 11:06:51 +0900

    first commit Sample

---
+++ src/main/java/spring_Git_Test/spring_Git_Test_hello/SpringGitTestHelloApplication.java
@@ -0,0 +1,13 @@
+package spring_Git_Test.spring_Git_Test_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitTestHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitTestHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_Git_Test/spring_Git_Test_hello/SpringGitTestHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_Git_Test.spring_Git_Test_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitTestHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
